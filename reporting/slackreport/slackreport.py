1c0e256260a18c7b87b6506cd8a83218b8764a8a
jmesa <jmesa@users.noreply.github.com>
2019-07-24 13:24:05 +0200

    Added PDF support

--- reporting/slackreport/slackreport.py
+++ reporting/slackreport/slackreport.py
@@ -1,13 +1,10 @@
 from __future__ import unicode_literals
 import json
-import os
 
 from fame.common.exceptions import ModuleInitializationError
 from fame.common.exceptions import ModuleExecutionError
 
 from fame.core.module import ReportingModule
-from fame.common.config import fame_config
-from fame.core.config import Config
 
 
 try:
@@ -15,6 +12,12 @@ try:
     HAVE_REQUESTS = True
 except ImportError:
     HAVE_REQUESTS = False
+
+try:
+    import WeasyPrint
+    HAVE_WeasyPrint = True
+except ImportError:
+    HAVE_WeasyPrint = False
 
 try:
     from defang import defang
@@ -53,6 +56,12 @@ class SlackReport(ReportingModule):
             'name': 'proxy',
             'type': 'str',
             'description': 'If you are behind a proxy, please provide it'
+        },
+        {
+            'name': 'pdfreport',
+            'type': 'bool',
+            'default': False,
+            'description': 'Enable PDF report instead of HTML'
         },        
     ]
 
@@ -63,6 +72,9 @@ class SlackReport(ReportingModule):
 
             if not HAVE_DEFANG:
                 raise ModuleInitializationError(self, "Missing dependency: defang")
+
+            if not HAVE_WeasyPrint:
+                raise ModuleInitializationError(self, "Missing dependency: WeasyPrint")                
 
             return True
         else:
@@ -76,19 +88,40 @@ class SlackReport(ReportingModule):
         response = requests.get(url_analysis, stream=True, headers={'X-API-KEY': self.fame_api_key})
         response.raise_for_status()
 
-        report = {
-          'file': ("{0}_report.html".format(analysis['_id']), response.content)
-          }
 
-        payload={
-          "title":"{0}_report.html".format(analysis['_id']),
-          "initial_comment": "Report of {0}\n<{1}/analyses/{2}|See the analysis on FAME>".format(
-            defang(', '.join(analysis._file['names'])),
-            self.fame_base_url,
-            analysis['_id']
-            ),
-          "token":self.legacy_token, 
-          "channels":self.channel, 
-        }
+        if self.pdfreport:
+        ### PDF
+            HTML(file_obj=response.content).write_pdf('/tmp/test.pdf')
+            report = {
+              'file': '/tmp/test.pdf'
+              }
 
-        r = requests.post("https://slack.com/api/files.upload", params=payload, files=report)
+            payload={
+              "title":"{0}_report.pdf".format(analysis['_id']),
+              "initial_comment": "Report of {0}\n<{1}/analyses/{2}|See the analysis on FAME>".format(
+                defang(', '.join(analysis._file['names'])),
+                self.fame_base_url,
+                analysis['_id']
+                ),
+              "token":self.legacy_token, 
+              "channels":self.channel, 
+            }            
+            r = requests.post("https://slack.com/api/files.upload", params=payload, files=report)            
+            
+        else:
+        ### HTML
+            report = {
+              'file': ("{0}_report.html".format(analysis['_id']), response.content)
+              }
+
+            payload={
+              "title":"{0}_report.html".format(analysis['_id']),
+              "initial_comment": "Report of {0}\n<{1}/analyses/{2}|See the analysis on FAME>".format(
+                defang(', '.join(analysis._file['names'])),
+                self.fame_base_url,
+                analysis['_id']
+                ),
+              "token":self.legacy_token, 
+              "channels":self.channel, 
+            }            
+            r = requests.post("https://slack.com/api/files.upload", params=payload, files=report)
